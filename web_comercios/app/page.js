cb6ba54337ead7a0c801b40b69ecec73fb93f5be
aperezmarquez <145431629+aperezmarquez@users.noreply.github.com>
2023-12-05 21:42:05 +0100

    Creado la tarjeta de best seller
    En la main page he añadido el objeto más vendido de la web con un enlace a el

--- web_comercios/app/page.js
+++ web_comercios/app/page.js
@@ -1,12 +1,10 @@
 import './page.css';
-import bestSellerImg from './imgs/arcade_machine.jpg';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className='fondo w-full h-screen grid sm:grid-cols-1 grid-cols-2 grid-flow-col'>
-      <div className='texto col-start-1'>
+      <div className='texto col-end-1'>
         <p className='main-text'>Find <span id='everything'>Everything</span> You 
                                       <br></br><span id='search'>Search</span> & <span id='more'>More</span>
                                       <br></br>With A Couple Clicks
@@ -18,8 +16,16 @@ export default function Home() {
           <Link id='commercesbutton' href='http://localhost:3000/comercios' className='boton'>Commerces</Link>
         </div>
       </div>
-      <div className='col-end-3'>
-        <Image src={bestSellerImg} alt='Best Seller' className='bestSellerImg rounded-2xl'></Image>
+      <div className='col-end-2'>
+        <div className='card rounded-xl'>
+          <div className='card-elements rounded-xl'>
+              <div className='card-elements-holder rounded-b-xl rounded-t-sm'>
+                <p className='card-text'>Best Seller</p>
+                <p className='card-description'>This is a description with some text to test it.</p>
+                <Link id='card-button' href='http://localhost:3000/sign-in' className='boton'>Visit Web</Link>
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   );
